91cf0c9c40da9268b96624d801f36389bfafd948
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-08-24 20:23:40 -0700

    Update content of Program.cs.

--- SaferSpaces/Program.cs
+++ SaferSpaces/Program.cs
@@ -14,14 +14,11 @@ namespace SaferSpacesClient
   {
     public static void Main(string[] args)
     {
-      var host = new WebHostBuilder()
-        .UseKestrel()
-        .UseContentRoot(Directory.GetCurrentDirectory())
-        .UseIISIntegration()
-        .UseStartup<Startup>()
-        .Build();
-
-      host.Run();
+      CreateWebHostBuilder(args).Build().Run();
     }
+
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
   }
 }
