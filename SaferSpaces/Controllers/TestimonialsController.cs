7ea8f6c2ec2a3ab87f0104314ff135273f31b304
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-08-24 19:01:20 -0700

    Add using statements and namespace to home and testimonials controller.

---
+++ SaferSpaces/Controllers/TestimonialsController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ExplorationClient.Models;
+
+namespace SaferSpacesClient.Controller
+{
+
+}
