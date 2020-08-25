207d4d83ce890e2d34ab69b3ff2eee438c03a144
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-08-25 08:58:21 -0700

    Update Event model.
    Co-authored-by: MarielHamson <mariel.hamson@gmail.com>
    Co-authored-by: RichterBros <erich.richter@gmail.com>
    Co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

--- SaferSpaces/Models/Event.cs
+++ SaferSpaces/Models/Event.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace SaferSpacesClient.Models
 {
@@ -29,9 +31,9 @@ namespace SaferSpacesClient.Models
       var result = apiCallTask.Result;
 
       JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
-      List<Event> eventList = JsonConvert.DeserializeObject<List<Event>>(jsonResponse.ToString());
+      List<Event> gatheringList = JsonConvert.DeserializeObject<List<Event>>(jsonResponse.ToString());
 
-      return eventList;
+      return gatheringList;
     }
 
     public static Event GetDetails(int id)
@@ -39,22 +41,22 @@ namespace SaferSpacesClient.Models
       var apiCallTask = ApiHelper.Get(id);
       var result = apiCallTask.Result;
       JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
-      Event happening = JsonConvert.DeserializeObject<Event>(jsonResponse.ToString());
-      return happening;
+      Event gathering = JsonConvert.DeserializeObject<Event>(jsonResponse.ToString());
+      return gathering;
     }
 
-    public static void Post(Event happening)
+    public static void Post(Event gathering)
     {
-      string jsonEvent = JsonConvert.SerializeObject(happening);
+      string jsonEvent = JsonConvert.SerializeObject(gathering);
       var apiCallTask = ApiHelper.Post(jsonEvent);
     }
 
-    public static void Put(Event happening)
+    public static void Put(Event gathering)
     {
-      string jsonEvent = JsonConvert.SerializeObject(happening);
-      var apiCallTask = ApiHelper.Put(happening.EventId, jsonEvent);
+      string jsonEvent = JsonConvert.SerializeObject(gathering);
+      var apiCallTask = ApiHelper.Put(gathering.EventId, jsonEvent);
     }
-    
+
     public static void Delete(int id)
     {
       var apiCallTask = ApiHelper.Delete(id);
