25d3d3476a7c7c60e19c0fd992fb66b1a37a9848
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-08-24 18:46:30 -0700

    Add Place.cs model.

---
+++ SaferSpaces/Models/Place.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SaferSpacesClient.Models
+{
+  public class Place
+  {
+    public Place()
+    {
+      this.Events = new HashSet<Event>();
+      this.Testimonials = new HashSet<Testimonial>();
+    }
+    public int PlaceId { get;set; }
+    public string Name { get;set; }
+    public string Type { get;set; }
+    public string Address { get;set; }
+    public Restroom RestroomFeatures { get;set; }
+    public virtual ICollection<Event> Events { get;set; }
+    public virtual ICollection<Testimonial> Testimonials { get; set; }
+
+    public static List<Place> GetPlaces()
+    {
+      var apiCallTask = ApiHelper.GetAll();
+      var result = apiCallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<Place> placeList = JsonConvert.DeserializeObject<List<Place>>(jsonResponse.ToString());
+
+      return placeList;
+    }
+
+    public static Place GetDetails(int id)
+    {
+      var apiCallTask = ApiHelper.Get(id);
+      var result = apiCallTask.Result;
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      Place place = JsonConvert.DeserializeObject<Place>(jsonResponse.ToString());
+      return place;
+    }
+
+    public static void Post(Place place)
+    {
+      string jsonPlace = JsonConvert.SerializeObject(place);
+      var apiCallTask = ApiHelper.Post(jsonPlace);
+    }
+
+    public static void Put(Place place)
+    {
+      string jsonPlace = JsonConvert.SerializeObject(place);
+      var apiCallTask = ApiHelper.Put(place.PlaceId, jsonPlace);
+    }
+    
+    public static void Delete(int id)
+    {
+      var apiCallTask = ApiHelper.Delete(id);
+    }
+  }
+}
