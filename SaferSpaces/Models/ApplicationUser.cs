5e22ea05c33a87612fb0873c9e54c3d7e164d612
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-08-27 09:49:31 -0700

    fix login/register with new migration

--- SaferSpaces/Models/ApplicationUser.cs
+++ SaferSpaces/Models/ApplicationUser.cs
@@ -4,8 +4,5 @@ namespace SaferSpacesClient.Models
 {
   public class ApplicationUser : IdentityUser
   {
-    // public string UserRole { get; set; } = "User";
   }
 }
-
-// https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.identity.identityuser.-ctor?view=aspnetcore-2.2
